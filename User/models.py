00435c6cfee9cb68b3c20c19af67f8c25b923a2a
sup1p <sup4anski@gmail.com>
2025-03-26 14:31:03 +0500

    starting the project

---
+++ User/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    email
+    pictire
+    last_seen
+    reputation
+    question_count
+    answer_count
+    comment_count
+    upvotes
+    downvotes
+    bio
